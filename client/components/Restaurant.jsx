ec8fa51f3d649dedf06647116234006562b1924e
su-kun1899 <higedrum.coz@gmail.com>
2022-11-07 01:34:14 +0900

    feat: :wrench: ひとまず表示できるようにする

--- client/components/Restaurant.jsx
+++ client/components/Restaurant.jsx
@@ -9,23 +9,23 @@ export function Restaurant({restaurant}) {
                         <img src={restaurant.image || "/images/restaurants/noimage.png"} alt={restaurant.name}/>
                     </figure>
                 </div>
-                <div className="column">
-                    <h3 className="title is-5">
-                        <Link className="has-text-dark" to={`/restaurants/${restaurant.id}`}>{restaurant.name}</Link>
-                    </h3>
-                    <div>
-                        {restaurant.reviews.length === 0 ? (
-                            <p>
-                                レビューがまだありません。<br/>
-                                <Link to={`/restaurants/${restaurant.id}`}>レビューを投稿する</Link>
-                            </p>
-                        ) : (
-                            restaurant.reviews.map((review) => {
-                                // return <R
-                            })
-                        )}
-                    </div>
-                </div>
+                {/*<div className="column">*/}
+                {/*    <h3 className="title is-5">*/}
+                {/*        <Link className="has-text-dark" to={`/restaurants/${restaurant.id}`}>{restaurant.name}</Link>*/}
+                {/*    </h3>*/}
+                {/*    <div>*/}
+                {/*        {restaurant.reviews.length === 0 ? (*/}
+                {/*            <p>*/}
+                {/*                レビューがまだありません。<br/>*/}
+                {/*                <Link to={`/restaurants/${restaurant.id}`}>レビューを投稿する</Link>*/}
+                {/*            </p>*/}
+                {/*        ) : (*/}
+                {/*            restaurant.reviews.map((review) => {*/}
+                {/*                // return <R*/}
+                {/*            })*/}
+                {/*        )}*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
         </article>
     )
