112b89600baefc834d7b1c051ae78e00daa0ef93
su-kun1899 <higedrum.coz@gmail.com>
2022-11-14 09:26:02 +0900

    feat: :wrench: 詳細画面にパンくずを表示

--- client/src/pages/RestaurantDetail.jsx
+++ client/src/pages/RestaurantDetail.jsx
@@ -1,3 +1,32 @@
+import {Breadcrumb} from "../components/Breadcrumb";
+import {useEffect, useState} from "react";
+import {useLocation, useParams} from "react-router-dom";
+import {getRestaurant} from "../api";
+
 export function RestaurantDetailPage() {
-    return <>工事中</>
+    const [restaurant, setRestaurant] = useState(null);
+
+    const params = useParams();
+
+    useEffect(() => {
+        getRestaurant(params.restaurantId).then((data) => {
+            setRestaurant(data);
+        });
+    }, [params.restaurantId]);
+
+    return (
+        <>
+            <div className="box">
+                <Breadcrumb links={[
+                    {href: "/", content: "Top"},
+                    {href: "/restaurants", content: "ラーメン店一覧"},
+                    {
+                        href: `/restaurants/${params.restaurantId}`,
+                        content: restaurant && `${restaurant.name} の情報`,
+                        active: true,
+                    },
+                ]}/>
+            </div>
+        </>
+    );
 }
