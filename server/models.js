b2e59a89aac6c5f7309e61c6878323e39b3f1562
su-kun1899 <higedrum.coz@gmail.com>
2022-11-19 20:55:16 +0900

    feat: :wrench: Restaurant と Review の model を追加

--- server/models.js
+++ server/models.js
@@ -16,5 +16,51 @@ export const User = sequelize.define(
             allowNull: false,
         },
     },
-    {underscored: true}
+    {underscored: true},
 )
+
+export const Restaurant = sequelize.define(
+    "restaurant",
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+        map: {
+            type: DataTypes.TEXT,
+        },
+    },
+    {underscored: true},
+);
+
+export const Review = sequelize.define(
+    "review",
+    {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+            },
+        },
+        restaurantId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Restaurant,
+            },
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {underscored: true},
+);
