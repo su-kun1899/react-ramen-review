0ce252b29d4d20ba69208bf71c0b28dd61e2199a
su-kun1899 <higedrum.coz@gmail.com>
2022-11-23 16:38:51 +0900

    feat: :wrench: auth0 の認証処理を作成

---
+++ server/auth0.js
@@ -0,0 +1,25 @@
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
+
+export const checkJwt = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-fva6jkbt.auth0.com/.well-known/jwks.json',
+    }),
+    audience: 'https://damp-beach-07590.herokuapp.com',
+    issuer: 'https://dev-fva6jkbt.auth0.com/',
+    algorithms: ['RS256']
+});
+
+export async function getUser(token) {
+    return await fetch(
+        "https://dev-fva6jkbt.auth0.com/userinfo",
+        {
+            headers: {
+                Authorization: token,
+            },
+        }
+    );
+}
